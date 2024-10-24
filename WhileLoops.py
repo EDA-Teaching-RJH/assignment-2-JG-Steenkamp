cee103f644da9bede4e8384436778e1346843077
JG-Steenkamp <js2436@kent.ac.uk>
2024-10-24 12:03:04 +0000

    fifnish

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,1 +1,15 @@
 ### Part Two -- your code goes here. 
+import random
+
+number = random.randint(1,100)
+
+guess = 0
+
+print("Lets play a little game, guess what number I'm thinking of.")
+
+while guess != number:
+
+    guess = int(input('guess: '))
+    if guess == number:
+        print("You got it!")
+        break
